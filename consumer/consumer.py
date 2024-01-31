2169df63d89245b42a63289a517127687ef8d734
alanCrtl <alan.courtel1999@gmail.com>
2024-01-31 18:21:27 +0100

    changed producer and consumer to work with python-kafka instead of confluent_kafka

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,7 +1,8 @@
 import psycopg2
 from psycopg2 import sql
 import requests
-from confluent_kafka import Consumer, KafkaError
+from kafka import KafkaConsumer
+
 class Coord:
     def __init__(self, lat, long, ip, date) -> None:
         self.lat: float = lat
@@ -27,28 +28,27 @@ def push_to_db(coord: Coord):
 
 # ==== main function ====
 def consume_messages(bootstrap_servers, group_id, topic):
+
     consumer_conf = {
-        'bootstrap.servers': bootstrap_servers,
-        'group.id': group_id,
-        'auto.offset.reset': 'earliest'
+        'bootstrap_servers': bootstrap_servers,
+        'group_id': group_id,
+        'enable_auto_commit': True
     }
 
-    consumer = Consumer(consumer_conf)
+    consumer = KafkaConsumer(topic, **consumer_conf)
     consumer.subscribe([topic])
     
     max_iterations_without_messages = 15
     iterations_without_messages = 0
 
-
-
-    print(f'bootstrapped the consumer to broker and topic [{topic}]; waiting for messages...')
     try:
         while True:
-            msg = consumer.poll(timeout=10)
+            print('waiting for messages...')
+            messages = consumer.poll(timeout_ms=4000)
             
-            # exit after too much idle time
-            if msg is None:
+            if not messages:
                 iterations_without_messages += 1
+                print('No messages received.')
                 if iterations_without_messages > max_iterations_without_messages:
                     print("No messages received for {} iterations. Exiting.".format(max_iterations_without_messages))
                     break
@@ -57,29 +57,11 @@ def consume_messages(bootstrap_servers, group_id, topic):
                 continue
 
             iterations_without_messages = 0  # Reset the count when a message is received
+            
+            for topic_partition, partition_messages in messages.items():
+                for msg in partition_messages:
+                    process_kafka_message(msg)
 
-            # error handling
-            if msg.error():
-                if msg.error().code() == KafkaError._PARTITION_EOF:
-                    print("Reached end of partition. Continuing.")
-                    continue
-                else:
-                    print("Error: {}".format(msg.error()))
-                    break
-            
-            # parse message
-            print(f'msg received: {msg.value().decode("utf-8")}')
-            splitted_message = msg.value().decode('utf-8').split('; ')
-            lat = float(splitted_message[0])
-            long = float(splitted_message[1])
-            ip = splitted_message[2]
-            date = splitted_message[3]
-
-            
-            # turn it into object and push to DB
-            coord = Coord(lat=lat, long=long, ip=ip, date=date)
-            push_to_db(coord)
-            
     except KeyboardInterrupt:
         print("KeyboardInterrupt")
         pass
@@ -87,6 +69,25 @@ def consume_messages(bootstrap_servers, group_id, topic):
     finally:
         print("Exiting the consumer.")
         consumer.close()
+
+def process_kafka_message(msg):
+    try:
+        # parse message
+        print(f'msg received: {msg.value.decode("utf-8")}')
+        splitted_message = msg.value.decode('utf-8').split(';')
+        lat = float(splitted_message[0])
+        long = float(splitted_message[1])
+        ip = splitted_message[2]
+        date = splitted_message[3]
+
+        # turn it into object and push to DB
+        coord = Coord(lat=lat, long=long, ip=ip, date=date)
+        print(f'coord : {coord}')
+        push_to_db(coord)
+
+    except Exception as e:
+        print(f"Error processing message: {e}")
+
 
 if __name__ == '__main__':
     bootstrap_servers = 'kafka:9092'  # Kafka broker's address
